1d7baa479c93a26fcfc8bce6e037484303a74455
Cerber-one <33935354+Cerber-one@users.noreply.github.com>
2024-06-11 16:44:35 +0600

    Update Select_ffom_chat_Youtube.user.js

--- Select_ffom_chat_Youtube.user.js
+++ Select_ffom_chat_Youtube.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Выборка из чата Youtube
 // @namespace    http://tampermonkey.net/
-// @version      0.3.2
+// @version      0.3.3
 // @description  try to take over the world!
 // @author       Crber
 // @match            *://*.youtube.com/live_chat*
